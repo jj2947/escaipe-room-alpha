6cfd4419eb3c2ee4989680f7600101fd3ff48b6b
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-08 15:48:29 +1200

    created new scenes for when the game is over

---
+++ src/main/java/nz/ac/auckland/se206/controllers/EndController.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class EndController {
+  @FXML private Button restartButton;
+
+  @FXML
+  public void clickRestart(MouseEvent event) throws IOException {}
+}
