71874d10425a36eb119e910f58ef74b674ee729a
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-09 21:10:28 +1200

    added methods for buttons

--- src/main/java/nz/ac/auckland/se206/controllers/PinpadController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/PinpadController.java
@@ -29,9 +29,11 @@ public class PinpadController {
   @FXML private Button eightButton;
   @FXML private Button nineButton;
   @FXML private Button enterButton;
-  @FXML private TextField pinpadTextField;
+  @FXML private TextField textField;
   @FXML private Label timerLabel3;
   @FXML private Button helpButton;
+  @FXML private Button backButton;
+  @FXML private Label chatLabel;
   private Timer timer;
   private ChatCompletionRequest chatCompletionRequest;
 
@@ -72,7 +74,8 @@ public class PinpadController {
 
               Platform.runLater(
                   () -> {
-                    pinpadTextField.setText(sentence);
+                    textField.setText("");
+                    chatLabel.setText(sentence);
                   });
 
               if (currentSentenceIndex < chatSentences.length - 1) {
@@ -120,8 +123,9 @@ public class PinpadController {
     // Disable the help button temporarily to prevent multiple clicks
     helpButton.setDisable(true);
 
+    textField.setText("");
     // Set the chat label to loading message
-    pinpadTextField.setText("Loading...");
+    chatLabel.setText("Loading...");
 
     // Run the AI chat in the background
     Task<Void> aiChatTask =
@@ -132,7 +136,7 @@ public class PinpadController {
               String helpMessage = getAIHelpMessage(); // Get AI-generated help message
               Platform.runLater(
                   () -> {
-                    pinpadTextField.setText(helpMessage);
+                    chatLabel.setText(helpMessage);
                   });
             } catch (Exception e) {
               e.printStackTrace();
@@ -163,5 +167,82 @@ public class PinpadController {
     Thread aiChatThread = new Thread(aiChatTask);
     aiChatThread.setDaemon(true);
     aiChatThread.start();
+  }
+
+  @FXML
+  public void onClickOne() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "1");
+  }
+
+  @FXML
+  public void onClickTwo() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "2");
+  }
+
+  @FXML
+  public void onClickThree() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "3");
+  }
+
+  @FXML
+  public void onClickFour() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "4");
+  }
+
+  @FXML
+  public void onClickFive() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "5");
+  }
+
+  @FXML
+  public void onClickSix() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "6");
+  }
+
+  @FXML
+  public void onClickSeven() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "7");
+  }
+
+  @FXML
+  public void onClickEight() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "8");
+  }
+
+  @FXML
+  public void onClickNine() {
+    chatLabel.setText("");
+    textField.setText(textField.getText() + "9");
+  }
+
+  @FXML
+  public void onEnter() {
+    if (textField.getText().equals(GameState.Code)) {
+      GameState.isTimeReached = true;
+      Scene currentScene = timerLabel3.getScene();
+      Platform.runLater(
+          () -> {
+            currentScene.setRoot(SceneManager.getUiRoot(AppUi.ESCAPED));
+          });
+    } else {
+      textField.setText("Incorrect");
+    }
+  }
+
+  @FXML
+  public void onBack() {
+    Scene currentScene = timerLabel3.getScene();
+    Platform.runLater(
+        () -> {
+          currentScene.setRoot(SceneManager.getUiRoot(AppUi.ROOM));
+        });
   }
 }
