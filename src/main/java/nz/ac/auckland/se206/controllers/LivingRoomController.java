d8af7ed4ae38413b62b485cef6132fee2f2f5d0a
Jessica Jiang <jessicaj2947@gmail.com>
2023-08-10 22:53:06 +1200

    made a new living room scene and changed the images for the scenes

---
+++ src/main/java/nz/ac/auckland/se206/controllers/LivingRoomController.java
@@ -0,0 +1,27 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LivingRoomController {
+    
+    @FXML private Button hintButton;
+    @FXML private Button backButton;
+    @FXML private Label timerLabel;
+    @FXML private Label chatLabel;
+
+    public void initialize() {
+        // Initialization code goes here
+    }
+
+    @FXML
+    public void onHint() {
+        // Hint button action code goes here
+    }
+
+    @FXML
+    public void onBack() {
+        // Back button action code goes here
+    }
+}
